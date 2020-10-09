3d501e6e6d1b91f0a93a164f6ccc1a554981f7b3
Jose Manuel Mancha Fernandez <jose.m.manchaf@udc.es>
2020-10-09 23:49:49 +0200

    changed way to get text channel to id

--- cogs/greetings.py
+++ cogs/greetings.py
@@ -8,18 +8,18 @@ class Greetings(commands.Cog):
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
-        channel = member.guild.system_channel
+        channel = self.bot.get_channel(761965902568488986)
         if channel is not None:
-            await channel.send('Welcome {0.mention}.'.format(member))
+            await channel.send('Bienvenido al servidor de GPUL {0.mention}.'.format(member))
 
     @commands.command()
     async def hello(self, ctx, *, member: discord.Member = None):
         """Says hello"""
         member = member or ctx.author
         if self._last_member is None or self._last_member.id != member.id:
-            await ctx.send('Hello {}'.format(member.mention))
+            await ctx.send('Hola {}'.format(member.mention))
         else:
-            await ctx.send('Hello {}... This feels familiar.'.format(member.mention))
+            await ctx.send('Hola {}... Esto me resulta familiar.'.format(member.mention))
         self._last_member = member
 
 def setup(bot):
