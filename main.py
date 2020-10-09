b1ce062710f0eae58944ae7b2642eb8224fceaab
Pedrety <pedrety998@gmail.com>
2020-10-09 21:14:50 +0200

    Added ready check when bot is running

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ from discord.ext import commands
 
 bot = commands.Bot(command_prefix='!')
 
+@bot.event
+async def on_ready():
+  print('Bot is ready!')
+
 bot.load_extension(f'cogs.test')
 
 bot.run('TOKEN')
