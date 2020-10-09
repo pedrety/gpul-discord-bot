731d233fa33abccd3c4f54abf17eaec76b255851
Sr.Bruno <sr.brunocabado@gmail.com>
2020-10-09 20:57:59 +0200

    loads all cogs files in main

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+import os
 from discord.ext import commands
 
 bot = commands.Bot(command_prefix='!')
 
-bot.load_extension(f'cogs.test')
+for filename in os.listdir("./cogs"):
+    if filename.endswith(".py") and filename != "__init__.py":
+        bot.load_extension(f'cogs.{filename[:-3]}')
 
 bot.run('TOKEN')
