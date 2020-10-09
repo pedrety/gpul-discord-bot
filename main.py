92de27b8a12fda64c9dcea7dfe7f1ce86869b419
Sr.Bruno <sr.brunocabado@gmail.com>
2020-10-09 21:46:47 +0200

    bug fix cogs load and env variable added

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import os
 from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
 
 bot = commands.Bot(command_prefix='!')
 
 @bot.event
 async def on_ready():
-  print('Bot is ready!')
+	print('Bot is ready!')
 
-bot.load_extension(f'cogs.test')
 for filename in os.listdir("./cogs"):
-    if filename.endswith(".py") and filename != "__init__.py":
-        bot.load_extension(f'cogs.{filename[:-3]}')
+	if filename.endswith(".py") and filename != "__init__.py":
+		bot.load_extension(f'cogs.{filename[:-3]}')
 
-bot.run('TOKEN')
+bot.run(TOKEN)
